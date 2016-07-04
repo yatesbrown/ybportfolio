2f5dee8ec3fceacdfda10467000998e973b74a15
Yates Brown <ybyates92@gmail.com>
2016-07-04 11:26:48 -0400

    made modifications to index

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+# get "/" do
+#   send_file File.join(settings.public_folder, 'hello.html')
+# end
+
 get "/" do
-  File.open("public/hello.html").read 
+  File.open("public/index.html").read
 end
